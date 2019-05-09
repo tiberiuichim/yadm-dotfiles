2ea0707f36b096d305f73492deb985de7b07a32a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-05-09 15:33:54 +0300

    Update tmux config file for version 2.9

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,5 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+# eval $HOME/tools3/bin/python -m virtualfish
