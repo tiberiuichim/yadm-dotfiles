89f3bf38bdcdc8b61ba5f5036a79646cab3bca26
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-11-12 11:01:42 +0200

    Add keys to move buffers around

--- .config/nvim-lazy/lua/config/keymaps.lua
+++ .config/nvim-lazy/lua/config/keymaps.lua
@@ -1,15 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
--- nmap <tab> :b#<cr>
--- " cycle through tabs with shift-tab
--- nmap <S-tab> :bn<cr>
 
 local map = vim.keymap.set
 
+-- " cycle through tabs with shift-tab
 map("n", "<tab>", "<cmd>b#<cr>", { desc = "Next buffer" })
 map("n", "<S-tab>", "<cmd>bn<cr>", { desc = "Next buffer" })
 
 -- Keep cursor at the bottom of the visual selection after you yank it.
 map("v", "y", "ygv<Esc>", { desc = "Yank visual selection" })
+
+map("n", "<S-Right>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer to right" })
+map("n", "<S-Left>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer to left" })
