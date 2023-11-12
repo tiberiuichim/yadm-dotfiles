f0c39dcc38c65308557939f7531392da1d48d8df
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-11-12 10:50:09 +0200

    Configure alduin

--- .config/nvim-lazy/lua/config/keymaps.lua
+++ .config/nvim-lazy/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- nmap <tab> :b#<cr>
+-- " cycle through tabs with shift-tab
+-- nmap <S-tab> :bn<cr>
+
+local map = vim.keymap.set
+
+map("n", "<tab>", "<cmd>b#<cr>", { desc = "Next buffer" })
+map("n", "<S-tab>", "<cmd>bn<cr>", { desc = "Next buffer" })
+
+-- Keep cursor at the bottom of the visual selection after you yank it.
+map("v", "y", "ygv<Esc>", { desc = "Yank visual selection" })
--- .config/nvim-lazy/lua/plugins/bufdelete.lua
+++ .config/nvim-lazy/lua/plugins/bufdelete.lua
@@ -7,5 +7,6 @@ return {
   "famiu/bufdelete.nvim",
   keys = {
     { "<leader>Q", "<cmd>Bdelete<cr>", "Quit/Delete a buffer" },
+    { "Q", "<cmd>Bdelete<cr>", "Quit/Delete a buffer" },
   },
 }
