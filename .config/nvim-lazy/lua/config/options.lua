1cffed79c12281e2ca7758ddb2693337fbaf0d7f
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-11-12 22:30:59 +0200

    Setup bufline

--- .config/nvim-lazy/lua/config/options.lua
+++ .config/nvim-lazy/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options _mochathat are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- To temporarily disable the mouse, hold down shift key
+-- vim.opt.mouse = ''
