7b427e9aaba875759b4e4b5eb01245a910c1b8bf
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-11-23 08:30:36 +0200

    Add noice customization

---
+++ .config/nvim-lazy/lua/plugins/noice.lua
@@ -0,0 +1,45 @@
+local width = 120
+
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = width,
+          height = "auto",
+        },
+        -- clean popup style
+        border = {
+          style = "none",
+          padding = { 2, 3 },
+        },
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = width,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
+  },
+}
