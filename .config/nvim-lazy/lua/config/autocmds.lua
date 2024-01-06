cec0b5062053b6bca0e64ae3fc1c9c7e77b29e17
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-06 08:34:58 +0200

    Treat .script files as lua

--- .config/nvim-lazy/lua/config/autocmds.lua
+++ .config/nvim-lazy/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- au BufRead,BufNewFile *.mine                set filetype=mine
+
+vim.api.nvim_exec(
+  [[
+  augroup filetype_mapping
+    autocmd!
+    autocmd BufRead,BufNewFile *.script set filetype=lua
+  augroup END
+]],
+  false
+)
