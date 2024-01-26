87c3d5c5ced26c9007c3fe71aee8c311a1a432cf
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-26 09:20:10 +0200

    Tweak mouse and clipboard

--- .config/nvim-lazy/lua/config/options.lua
+++ .config/nvim-lazy/lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options _mochathat are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- To temporarily disable the mouse, hold down shift key
--- vim.opt.mouse = ''
-
 local opt = vim.opt
 local g = vim.g
 
@@ -15,5 +12,7 @@ opt.colorcolumn = "80"
 opt.relativenumber = false
 opt.ignorecase = false
 opt.conceallevel = 0
+opt.mouse = ""
+opt.clipboard = "unnamed"
 
 -- vim.opt.colorcolumn = "+1"
