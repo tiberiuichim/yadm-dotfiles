b71438697f2fe4560ae88cde0f4086038e91ffa9
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-06-20 13:18:12 +0300

    Add grug-far

---
+++ .config/nvim-lazy/lua/plugins/grug-far.lua
@@ -0,0 +1,18 @@
+local function global_search_replace()
+  local far = require("grug-far")
+  far.grug_far({ prefills = { search = vim.fn.expand("<cword>") } })
+end
+
+return {
+  "MagicDuck/grug-far.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>gy", global_search_replace, desc = "Global search/replace for word" },
+  },
+  config = function()
+    require("grug-far").setup({
+      -- ... options, see Configuration section below ...
+      -- ... there are no required options atm...
+    })
+  end,
+}
